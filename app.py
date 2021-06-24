5c647d7f9b498bd64cf7ca66a6303f16c4dfa04d
Mahajan <mahesh.mahajan@capgemini.com>
2021-06-24 18:05:10 +0530

    first comit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, url_for,request,redirect, session
+from selenium import webdriver
+import sys
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+app = Flask(__name__)
+app.secret_key =  'mahesh'
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/testing")
+def testing():
+    print("here")
+    chrome_options = Options()
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"]) 
+    driver = webdriver.Chrome(options = chrome_options)
+    driver.get('https://www.google.co.in/')
+    driver.find_element_by_name("q").send_keys("mahesh")
+    return "Success"
+
+if __name__ == "__main__":
+    app.run(debug=True)
